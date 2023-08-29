547275636ad418f023526666b63dbc4731db7506
santu97 <santhoshbhattaram@gmail.com>
2023-08-29 12:21:23 -0500

    Add project files.

---
+++ RazorPages/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorPages.Models;
+
+
+
+namespace RazorPages.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder model)
+        {
+            model.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 2 },
+                new Category { Id = 2, Name = "Drama", DisplayOrder = 1 },
+                new Category { Id = 3, Name = "Horror", DisplayOrder = 3 }
+            );
+        }
+    }
+}
---
+++ RazorPages/Models/Category.cs
@@ -0,0 +1,27 @@
+﻿
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+
+/*
+ * what to learn in this page??
+Validations, Model Creation, Database schema (key), required for not null and html helpers for display orders 
+
+ */
+
+namespace RazorPages.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Category Name should be mandatory")]
+        [DisplayName("Category Name")]
+        [MaxLength(30,ErrorMessage ="Maximum Length of Category Name should be 30")]
+        public String  Name{ get; set; }
+        //for displaying in html by using html helper(html for)
+        [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage ="Display Order should be between 1 to 100")]
+        public int DisplayOrder { get; set; }
+    }
+}
